519c1820601f056cbdbaeb4e7d913e42dc7500ce
Lalla <31769725+lallamilagro@users.noreply.github.com>
2018-05-05 02:18:35 +1000

    Create homepage with simple template
    The first version of the working page (without content)

--- ms/blog/tests.py
+++ ms/blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHomePage(TestCase):
+
+    def test_homepage_exists(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+  
+class TestBlogPage(TestCase):
+
+    def test_page_exists(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contains_expected_content(self):
+        response = self.client.get('/blog/')
+        self.assertContains(response, 'Hello hello!')
+
