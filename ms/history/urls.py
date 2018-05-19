6e1dbc52463285194d90e7fd1136c48f73ed4e4f
lallamilagro <for.lenochka@gmail.com>
2018-05-20 01:49:36 +1000

    Corrected references

--- ms/history/urls.py
+++ ms/history/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'history'
 urlpatterns = [
-    path('', views.HistoryView.as_view(), name='history'),
+    path('', views.HistoryView.as_view(), name="index"),
 ]
-
